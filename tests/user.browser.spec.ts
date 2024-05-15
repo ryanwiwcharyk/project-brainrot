6f01c75f449c7a2fa229b96918b2762ac3359d3e
ryanwiwcharyk <124375048+ryanwiwcharyk@users.noreply.github.com>
2024-05-15 19:16:09 +0000

    starting playwright tests

---
+++ tests/user.browser.spec.ts
@@ -0,0 +1,202 @@
+import postgres from "postgres";
+import { test, expect, Page } from "@playwright/test";
+import { getPath } from "../src/url";
+import User, {UserProps} from "../src/models/User";
+import { createUTCDate } from "../src/utils";
+
+const sql = postgres({
+	database: "UserStats",
+});
+
+const logout = async (page: Page) => {
+	await page.goto("/logout");
+};
+
+test.beforeEach(async () => {
+	// Anything you want to do before each test runs?
+});
+
+/**
+ * Clean up the database after each test. This function deletes all the rows
+ * from the todos and subtodos tables and resets the sequence for each table.
+ * @see https://www.postgresql.org/docs/13/sql-altersequence.html
+ */
+test.afterEach(async ({ page }) => {
+	// Replace the table_name with the name of the table(s) you want to clean up.
+	const tables = ["users"];
+
+	try {
+		for (const table of tables) {
+			await sql.unsafe(`DELETE FROM ${table}`);
+			await sql.unsafe(`ALTER SEQUENCE ${table}_id_seq RESTART WITH 1;`);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+
+	await logout(page);
+});
+
+const createUser = async (props: Partial<UserProps> = {}) => {
+	return await User.create(sql, {
+		userName: props.userName || "default",
+		email: props.email || "user@email.com",
+		password: props.password || "password",
+		createdAt: props.createdAt || createUTCDate(),
+	});
+};
+
+test("Homepage was retrieved successfully", async ({ page }) => {
+	await page.goto("/search");
+
+	expect(await page?.title()).toBe("Project Brainrot");
+});
+
+test("User was registered.", async ({ page }) => {
+	await page.goto(`register`);
+
+	await page.fill('form#register-form input[name="email"]', "user@email.com");
+	await page.fill('form#register-form input[name="password"]', "Password123");
+	await page.fill(
+		'form#register-form input[name="confirmPassword"]',
+		"Password123",
+	);
+	await page.click("form#register-form #register-form-submit-button");
+
+	expect(await page?.url()).toBe(getPath("login"));
+});
+
+test("User was not registered with blank email.", async ({ page }) => {
+	await page.goto(`register`);
+
+	await page.fill('form#register-form input[name="password"]', "Password123");
+	await page.fill(
+		'form#register-form input[name="confirmPassword"]',
+		"Password123",
+	);
+	await page.click("form#register-form #register-form-submit-button");
+
+	expect(await page?.url()).toMatch(getPath("register"));
+
+	const errorElement = await page.$("#error");
+
+	expect(await errorElement?.innerText()).toMatch("Email is required");
+});
+
+test("User was not registered with mismatched passwords.", async ({ page }) => {
+	await page.goto(`register`);
+
+	await page.fill('form#register-form input[name="email"]', "user@email.com");
+	await page.fill('form#register-form input[name="password"]', "Password123");
+	await page.fill(
+		'form#register-form input[name="confirmPassword"]',
+		"Password124",
+	);
+	await page.click("form#register-form #register-form-submit-button");
+
+	expect(await page?.url()).toMatch(getPath("register"));
+
+	const errorElement = await page.$("#error");
+
+	expect(await errorElement?.innerText()).toMatch("Passwords do not match");
+});
+
+test("User was logged in.", async ({ page }) => {
+	const user = await createUser({ password: "Password123" });
+
+	await page.goto(`/`);
+
+	let loginElement = await page.$(`nav a[href="${getPath("login")}"]`);
+	let logoutElement = await page.$(`nav a[href="${getPath("logout")}"]`);
+
+	expect(await loginElement).toBeTruthy();
+	expect(await logoutElement).toBeFalsy();
+
+	await loginElement?.click();
+
+	await page.fill('form#login-form input[name="email"]', user.props.email);
+	await page.fill('form#login-form input[name="password"]', "Password123");
+	await page.click("form#login-form #login-form-submit-button");
+
+	expect(await page?.url()).toBe(getPath("todos"));
+
+	loginElement = await page.$(`nav a[href="${getPath("login")}"]`);
+	logoutElement = await page.$(`nav a[href="${getPath("logout")}"]`);
+
+	expect(await loginElement).toBeFalsy();
+	expect(await logoutElement).toBeTruthy();
+});
+
+test("User was not logged in with blank email.", async ({ page }) => {
+	const user = await createUser({ password: "Password123" });
+
+	await page.goto(`/login`);
+
+	await page.fill('form#login-form input[name="password"]', "Password123");
+	await page.click("form#login-form #login-form-submit-button");
+
+	expect(await page?.url()).toMatch(getPath("login"));
+
+	const errorElement = await page.$("#error");
+
+	expect(await errorElement?.innerText()).toMatch("Email is required.");
+});
+
+test("User was not logged in with incorrect password.", async ({ page }) => {
+	const user = await createUser({ password: "Password123" });
+
+	await page.goto(`/login`);
+
+	await page.fill('form#login-form input[name="email"]', user.props.email);
+	await page.fill('form#login-form input[name="password"]', "Password124");
+	await page.click("form#login-form #login-form-submit-button");
+
+	expect(await page?.url()).toMatch(getPath("login"));
+
+	const errorElement = await page.$("#error");
+
+	expect(await errorElement?.innerText()).toMatch("Invalid credentials.");
+});
+
+test("User was logged out.", async ({ page, context }) => {
+	const user = await createUser({ password: "Password123" });
+
+	expect((await context.cookies()).length).toBe(0);
+
+	await page.goto(`/login`);
+
+	expect((await context.cookies()).length).toBe(1);
+
+	await page.fill('form#login-form input[name="email"]', user.props.email);
+	await page.fill('form#login-form input[name="password"]', "Password123");
+	await page.click("form#login-form #login-form-submit-button");
+
+	expect(await page?.url()).toBe(getPath("todos"));
+
+	const logoutElement = await page.$(`nav a[href="${getPath("logout")}"]`);
+
+	await logoutElement?.click();
+
+	expect(await page?.url()).toBe(getPath(""));
+
+	const loginElement = await page.$(`nav a[href="${getPath("login")}"]`);
+
+	expect(await loginElement).toBeTruthy();
+});
+
+test("User's email was remembered.", async ({ page }) => {
+	const user = await createUser({ email: "user@email.com" });
+	await page.goto(`/login`);
+
+	await page.fill('form#login-form input[name="email"]', user.props.email);
+	await page.fill('form#login-form input[name="password"]', "password");
+	await page.check('form#login-form input[name="remember"]');
+	await page.click("form#login-form #login-form-submit-button");
+
+	const cookies = await page.context().cookies();
+
+	const emailCookie = cookies.find((cookie) => cookie.name === "email");
+
+	expect(emailCookie).toBeTruthy();
+	expect(emailCookie?.value).toBe(user.props.email);
+});
